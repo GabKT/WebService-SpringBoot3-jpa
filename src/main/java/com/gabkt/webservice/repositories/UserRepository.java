624b20feb930f926a2d93478a6186c1724a62f93
Gabriel Barros <ktbarros07@gmail.com>
2024-01-25 23:47:48 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/gabkt/webservice/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.gabkt.webservice.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gabkt.webservice.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
