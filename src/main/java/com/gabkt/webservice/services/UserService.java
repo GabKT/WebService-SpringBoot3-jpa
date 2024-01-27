01babf2800308e41be52813eb0de0b54398561df
Gabriel Barros <ktbarros07@gmail.com>
2024-01-27 16:31:08 -0300

    Service layer, component registration

---
+++ src/main/java/com/gabkt/webservice/services/UserService.java
@@ -0,0 +1,26 @@
+package com.gabkt.webservice.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabkt.webservice.entities.User;
+import com.gabkt.webservice.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
