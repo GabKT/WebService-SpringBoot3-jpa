eff6f440d3c2a2a7b45da907ade4295804776898
Gabriel Barros <ktbarros07@gmail.com>
2024-01-31 18:52:23 -0300

    Order, Instant ISO 8601 - PART (2/2)

--- src/main/java/com/gabkt/webservice/config/TestConfig.java
+++ src/main/java/com/gabkt/webservice/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.gabkt.webservice.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.gabkt.webservice.entities.Order;
 import com.gabkt.webservice.entities.User;
+import com.gabkt.webservice.repositories.OrderRepository;
 import com.gabkt.webservice.repositories.UserRepository;
 
 @Configuration @Profile("test")
@@ -15,6 +18,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -22,7 +28,12 @@ public class TestConfig implements CommandLineRunner {
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		User u3 = new User(null, "Josilene", "josilene@email.com", "00000000", "123456");
 		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}
 	
 	
