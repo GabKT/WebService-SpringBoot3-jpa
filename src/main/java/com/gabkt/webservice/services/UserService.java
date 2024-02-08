5da77641aaf84a88b31be367d949c5a176d364a0
Gabriel Barros <ktbarros07@gmail.com>
2024-02-08 20:14:33 -0300

    Exception Handling - findById

--- src/main/java/com/gabkt/webservice/services/UserService.java
+++ src/main/java/com/gabkt/webservice/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.gabkt.webservice.entities.User;
 import com.gabkt.webservice.repositories.UserRepository;
+import com.gabkt.webservice.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
