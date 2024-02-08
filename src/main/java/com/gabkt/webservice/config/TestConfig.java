84cd56ba84c6e994e17d184d46830710ac740bf1
Gabriel Barros <ktbarros07@gmail.com>
2024-02-08 18:20:31 -0300

    Payment, one-to-one association

--- src/main/java/com/gabkt/webservice/config/TestConfig.java
+++ src/main/java/com/gabkt/webservice/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.gabkt.webservice.entities.Category;
 import com.gabkt.webservice.entities.Order;
 import com.gabkt.webservice.entities.OrderItem;
+import com.gabkt.webservice.entities.Payment;
 import com.gabkt.webservice.entities.Product;
 import com.gabkt.webservice.entities.User;
 import com.gabkt.webservice.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }
